f209404d9364945c32a3b9ad4aee4ded2cefeaa2
Chandler Mayo <chandler@chandlermayo.com>
2017-04-19 18:47:41 -0400

    Better airbnb png.

--- web/js/Components/Header.js
+++ web/js/Components/Header.js
@@ -18,7 +18,7 @@ const Header = (props)=> (
   <header style={styles.header}>
     <img
       style={styles.airbnb_logo}
-      src="http://logok.org/wp-content/uploads/2014/07/Airbnb-new-logo-2014.png" />
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2000px-Airbnb_Logo_B%C3%A9lo.svg.png" />
      <div className={styles.div}></div>
   </header>
 );
