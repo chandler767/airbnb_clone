f03f14e850cb72b09bd27387a3650a01a28e39a5
Chandler Mayo <chandler@chandlermayo.com>
2017-04-20 10:02:42 -0400

    Simplified states function.

--- web/js/Actions/StateActionCreators.js
+++ web/js/Actions/StateActionCreators.js
@@ -9,33 +9,34 @@ import {
 
 import request from 'superagent';
 
-export function RequestFinished(response, error) {
-        if error == null {
+export function fetchStateChange(response, error) {
+    if error == null {
+        if response == null { 
+            return { // If nothing was passed the the request was started.
+                type: REQUEST_INITIATED,
+            };
+        } else {
             return {
-            type: REQUEST_COMPLETED,
-            payload: response.data // Response
-        };
+                type: REQUEST_COMPLETED,
+                payload: response.data // Response
+            };
+        }
     } else {
         return {
             type: REQUEST_ERROR, // Error
             error
-        };
-    }
+        };   
+    }    
 }
 
 export function fetchStates() { // Make request to api
     return dispatch => {
-        dispatch(requestInitiated());
+        dispatch(fetchStateChange(null, null));
         request.get("http://localhost:3306/states/").then((data) => {
-                dispatch(RequestFinished(data.body, null));
+                dispatch(fetchStateChange(data.body, null));
             }).catch((error) => {
-                dispatch(RequestFinished(null, error));
+                dispatch(fetchStateChange(null, error));
             });
     };
 }
 
-export function requestInitiated() {
-    return {
-        type: REQUEST_INITIATED,
-    };
-}
